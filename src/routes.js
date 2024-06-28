fb1d910cdf81cadbb83b8dcbb36e3ecbdcb56d13
emanueleflor <emanueleflordarosa@gmail.com>
2024-06-28 14:55:36 -0300

    feat: add update and delete routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select("users");
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -31,10 +32,30 @@ export const routes = [
     },
   },
   {
-    method: "DELETE",
-    path: "/users/:id",
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
     handler: (req, res) => {
-      return res.end();
+      const { id } = req.params;
+      const { name, email } = req.body;
+
+      database.update("users", id, {
+        name,
+        email,
+      });
+
+      return res.writeHead(204).end();
     },
   },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete("users", id);
+
+      return res.writeHead(204).end();
+    },
+  },
+  ,
 ];
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,9 @@
+// /users/:id => expressao regular para encontrar textos dentro de um fromato especificos dentro de textos maiores
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
+  const pathWithParams = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9-_]+)");
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  return pathRegex;
+}
